4a6319b7ea812584c89b0a13abb1e09bfd4f92ea
VikasJadhav <jadhavvikas1404@gmail.com>
2022-12-17 16:13:08 +0530

    This is modified list program

--- list.py
+++ list.py
@@ -1,7 +1,9 @@
 list1 = ["apple", "banana", "cherry"]
 list2 = [1, 5, 7, 9, 3]
 list3 = [True, False, False]
+list4 = ["vikas", "vishal", "varad"]
 
 print(list1)
 print(list2)
 print(list3)
+print(list4)
