44ed9c0f44156de389ba8335dd99019a0e1008df
VikasJadhav8212 <120233936+VikasJadhav8212@users.noreply.github.com>
2023-11-10 11:32:32 +0530

    Update list.py

--- list.py
+++ list.py
@@ -2,10 +2,10 @@ list1 = ["apple", "banana", "cherry"]
 list2 = [1, 5, 7, 9, 3]
 list3 = [True, False, False]
 list4 = ["vikas", "vishal", "varad"]
-list5 = ["EC2", "S3", "VPC", "RDs"]
+
 
 print(list1)
 print(list2)
 print(list3)
 print(list4)
-print(list5)
+
