6346a54ee8eddc9fc4bb1ec7afc799d7ac42466b
VikasJadhav8212 <120233936+VikasJadhav8212@users.noreply.github.com>
2023-11-10 11:55:24 +0530

    Update list.py

--- list.py
+++ list.py
@@ -3,9 +3,11 @@ list2 = [1, 5, 7, 9, 3]
 list3 = [True, False, False]
 list4 = ["vikas", "vishal", "varad"]
 list5 = ["Amazon", "Flipcart:, "Python"]
+list6 = [23, 43, 2]
 
 print(list1)
 print(list2)
 print(list3)
 print(list4)
-
+print(list5)
+print(list6)
