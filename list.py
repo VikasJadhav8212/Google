5f9aca55ed04de500513848904c6a87bfc467a82
VikasJadhav8212 <120233936+VikasJadhav8212@users.noreply.github.com>
2023-11-10 11:50:47 +0530

    Update list.py
    New Commit-1

--- list.py
+++ list.py
@@ -2,7 +2,7 @@ list1 = ["apple", "banana", "cherry"]
 list2 = [1, 5, 7, 9, 3]
 list3 = [True, False, False]
 list4 = ["vikas", "vishal", "varad"]
-
+list5 = ["Amazon", "Flipcart:, "Python"]
 
 print(list1)
 print(list2)
