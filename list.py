08f8934dd14b0121fe9d6c8815d4b5cbfa67aaf6
VikasJadhav8212 <120233936+VikasJadhav8212@users.noreply.github.com>
2024-02-08 15:28:08 +0530

    Update list.py
    Added one list and print command

--- list.py
+++ list.py
@@ -4,10 +4,11 @@ list3 = [True, False, False]
 list4 = ["vikas", "vishal", "varad"]
 list5 = ["Amazon", "Flipcart:, "Python"]
 list6 = [23, 43, 2]
-
+list7 = [Aws, Azure, Alibaba]
 print(list1)
 print(list2)
 print(list3)
 print(list4)
 print(list5)
 print(list6)
+print)list7)
